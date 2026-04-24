7834bd10c6f93d927ed0162e6b2ccffedec2375d
Mashiro-chan <11561006+mashirochan@users.noreply.github.com>
2026-04-23 21:33:48 -0400

    Add .gitignore

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import vuetify from 'eslint-config-vuetify';
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default vuetify({
+   ts: true,
+});
+
+export default [
+   eslintConfigPrettier
+];
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-app-bar>
+        <template v-slot:prepend>
+          <v-icon icon="mdi-infinity" class="m-2" />
+        </template>
+
+        <v-app-bar-title>
+          infiniti <span class="text-red-600">red</span>
+        </v-app-bar-title>
+
+        <template v-slot:append>
+          <v-btn
+            icon="mdi-theme-light-dark"
+            class="m-2"
+            @click="$vuetify.theme.cycle()"
+          />
+        </template>
+      </v-app-bar>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup></script>
